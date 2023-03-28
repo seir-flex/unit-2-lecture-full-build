424c63e1d5ea458f2574ab34d5f85a27b17a0f6c
Joe Keohan <jkeohan@gmail.com>
2023-03-28 19:46:46 -0400

    initial commit

---
+++ starter_code/server.js
@@ -0,0 +1,16 @@
+// Dependencies
+const express = require('express');
+const app = express();
+const PORT = 3000
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.send('default route')
+})
+
+const tweetsController = require('./controllers/tweets');
+app.use('/tweets', tweetsController);
+
+// Listener
+app.listen(PORT, () => console.log(`express is listening on port: ${PORT}`));
