4046655461b9fc28121d9faf0e1f5ce829aa3dbf
Joe Keohan <jkeohan@gmail.com>
2023-03-31 18:32:47 -0400

    completed up to but not including auth

--- solution_code/controllers/fruits.js
+++ solution_code/controllers/fruits.js
@@ -3,15 +3,23 @@ const router = express.Router();
 const startFruits = require('../db/fruitSeedData.js')
 const Fruit = require('../models/fruit.js')
 
+// 
 router.post('/', async (req, res) => {
+	// check if the readyToEat property should be true or false
+	req.body.readyToEat = req.body.readyToEat === 'on' ? true : false;
 	const fruit = await Fruit.create(req.body);
-	res.send(fruit);
+	res.redirect('/fruits');
+});
+
+router.get('/new', async (req, res) => {
+	res.render("fruits/new.ejs")
 });
 
 // Index
 router.get('/', async (req, res) => {
 	const fruits = await Fruit.find({});
-	res.send(fruits);
+	// res.send(fruits);
+	res.render('fruits/index.ejs', { fruits });
 });
 
 // Seed
@@ -24,21 +32,31 @@ router.get('/seed', async (req, res) => {
 // Show
 router.get('/:id', async (req, res) => {
 	const fruit = await Fruit.findById(req.params.id);
-	res.send(fruit);
+	console.log('fruit', fruit)
+	res.render('fruits/show.ejs', { fruit });
 });
 
 // Delete
 router.delete('/:id', async (req, res) => {
 	const fruit = await Fruit.findByIdAndDelete(req.params.id);
-	res.send({ success: true, fruit });
+	res.redirect('/fruits');
 });
+
+// Edit
+
+router.get('/:id/edit', async (req, res) => {
+	const fruit = await Fruit.findById(req.params.id);
+	res.render('fruits/edit.ejs', { fruit });
+})
 
 // Update
 router.put('/:id', async (req, res) => {
+	req.body.readyToEat = req.body.readyToEat === 'on' ? true : false;
+
 	const fruit = await Fruit.findByIdAndUpdate(req.params.id, req.body, {
 		new: true,
 	});
-	res.send(fruit);
+	res.redirect('/fruits');
 });
 
 module.exports = router;
--- solution_code/server.js
+++ solution_code/server.js
@@ -11,6 +11,7 @@ require('dotenv').config();
 app.use(morgan("tiny")) //logging
 app.use(methodOverride("_method")) // override for put and delete requests from forms
 app.use(express.urlencoded({extended: true})) // parse urlencoded request bodies
+app.set('view engine', 'ejs');
 app.use(express.static("public")) // serve files from public statically
 
 app.get('/', (req, res) => {
