62d856a823c0ae4ab9a3003f8cd533efa7f6fcf3
Joe Keohan <jkeohan@gmail.com>
2023-04-01 16:35:57 -0400

    upto new.ejs

--- solution_code/controllers/fruits.js
+++ solution_code/controllers/fruits.js
@@ -3,14 +3,24 @@ const router = express.Router();
 const startFruits = require('../db/fruitSeedData.js')
 const Fruit = require('../models/fruit.js')
 
+// Post
 router.post('/', async (req, res) => {
 	const fruit = await Fruit.create(req.body);
 	res.send(fruit);
 });
 
+// New
+router.get('/new', (req, res) => {
+ res.render("fruits/new.ejs")
+})
+
 // Index...show all fruits
 router.get('/', async (req, res) => {
+	// wait or this to complete
+	// Fruit.find() is a Promise
+	// Promise is resolved or rejected
 	const fruits = await Fruit.find({});
+	// then run the next line of code
 	// res.send(fruits);
 	res.render("fruits/index.ejs", {fruits});
 });
--- solution_code/models/fruit.js
+++ solution_code/models/fruit.js
@@ -4,7 +4,7 @@ const fruitsSchema = new mongoose.Schema({
     name: String,
     color: String,
     readyToEat: Boolean
-})
+}, {timestamps: true})
 
 const Fruit = mongoose.model('Fruit', fruitsSchema)
 
