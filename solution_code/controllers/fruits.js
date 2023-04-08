35ec3fca8c9d589b5f8474283b46fe40bdf2a040
EricFithian <ef2413@columbia.edu>
2023-04-08 13:04:06 -0400

    Updated with user auth and sessions

--- solution_code/controllers/fruits.js
+++ solution_code/controllers/fruits.js
@@ -24,13 +24,14 @@ router.get('/:id/edit', async (req, res) => {
 
 // Index...show all fruits
 router.get('/', async (req, res) => {
+	console.log(req.session);
 	// wait or this to complete
 	// Fruit.find() is a Promise
 	// Promise is resolved or rejected
 	const fruits = await Fruit.find({});
 	// then run the next line of code
 	// res.send(fruits);
-	res.render("fruits/index.ejs", {fruits});
+	res.render("fruits/index.ejs", {fruits, user: req.session.currentUser.username});
 });
 
 // Seed
--- solution_code/server.js
+++ solution_code/server.js
@@ -4,11 +4,31 @@ const express = require('express');
 const morgan = require('morgan'); 
 const methodOverride = require('method-override');
 const app = express();
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 
+app.use(
+    session({
+        // The store needs to know that it's a mongo database and it needs access to the databases' connection
+        store: MongoStore.create({ 
+            mongoUrl: process.env.DATABASE_URL 
+        }),
+        // The secret ensures it's not some outside attack and it signs every session
+        secret: process.env.SECRET,
+        // No resaving the same same session and saving unitialized sessions
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 30
+            // This set the cookie to last for 30 days because it's in milliseconds
+        }
+    }),
+)
 /////////////////////////////////////////////////////
 // Middleware  req => middleware => res
 /////////////////////////////////////////////////////
+app.set('view engine', 'ejs')
 app.use(morgan("tiny")) //logging// 
 app.use(methodOverride("_method")) // override for put and delete requests from forms
 app.use(express.urlencoded({extended: true})) // parse urlencoded request bodies
@@ -19,7 +39,9 @@ app.get('/', (req, res) => {
 })
 
 const fruitsController = require('./controllers/fruits');
+const userController = require('./controllers/users')
 app.use('/fruits', fruitsController);
+app.use('', userController);
 
 // Listener
 app.listen(process.env.PORT, () =>
